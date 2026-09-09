d6b3049d4af0d1a36db17af75d63811184935c60
Cho-Jeonghyeon <69703120+Cho-Jeonghyeon@users.noreply.github.com>
2026-09-09 11:59:29 +0900

    Lab03 점 리스트 덱 기본 구조 정리

--- Lab_03/main.cpp
+++ Lab_03/main.cpp
@@ -1,105 +1,68 @@
 #include <iostream>
-
-#define SIZE 5
+#include <string>
 
 using namespace std;
 
-typedef struct {
-	int data[SIZE];
-	int front, rear;
-}Deque;
+const int SIZE = 10;
 
-//void error(char* message)
-//{
-//	fprintf(stderr, "%s\n", message);
-//	exit(1);
-//}
+struct Point {
+    int x;
+    int y;
+    int z;
+};
 
-void init_deque(Deque* q) {
-	q->front = q->rear = 0;
+struct Deque {
+    Point data[SIZE];
+    bool used[SIZE];
+    int front;
+    int rear;
+    int count;
+};
+
+void init_deque(Deque& q) {
+    q.front = 0;
+    q.rear = 0;
+    q.count = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+        q.used[i] = false;
+    }
 }
 
-int is_empty(Deque* q) {
-	return (q->front == q->rear);
+bool is_empty(const Deque& q) {
+    return q.count == 0;
 }
 
-int is_full(Deque* q) {
-	return ((q->rear + 1) % SIZE == q->front);
+bool is_full(const Deque& q) {
+    return q.count == SIZE;
 }
 
-//void deque_print(DequeType* q)
-//{
-//	printf("DEQUE(front=%d rear=%d) = ", q->front, q->rear);
-//	if (!is_empty(q)) {
-//		int i = q->front;
-//		do {
-//			i = (i + 1) % (MAX_QUEUE_SIZE);
-//			printf("%d | ", q->data[i]);
-//			if (i == q->rear)
-//				break;
-//		} while (i != q->front);
-//	}
-//	printf("\n");
-//}
-
-// 삽입 함수
-void add_rear(DequeType* q, element item)
-{
-	if (is_full(q))
-		error("큐가 포화상태입니다");
-	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
-	q->data[q->rear] = item;
-}
-// 삭제 함수
-element delete_front(DequeType* q)
-{
-	if (is_empty(q))
-		error("큐가 공백상태입니다");
-	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
-	return q->data[q->front];
+void error(const string& message) {
+    cout << message << endl;
 }
 
-// 삭제 함수
-element get_front(DequeType* q)
-{
-	if (is_empty(q))
-		error("큐가 공백상태입니다");
-	return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
-}
-void add_front(DequeType* q, element val)
-{
-	if (is_full(q))
-		error("큐가 포화상태입니다");
-	q->data[q->front] = val;
-	q->front = (q->front - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
+void print_point(const Point& point) {
+    cout << point.x << " " << point.y << " " << point.z;
 }
 
-element delete_rear(DequeType* q)
-{
-	int prev = q->rear;
-	if (is_empty(q))
-		error("큐가 공백상태입니다");
-	q->rear = (q->rear - 1 + MAX_QUEUE_SIZE) % MAX_QUEUE_SIZE;
-	return q->data[prev];
-}
-element get_rear(DequeType* q)
-{
-	if (is_empty(q))
-		error("큐가 공백상태입니다");
-	return q->data[q->rear];
+void deque_print(const Deque& q) {
+    for (int i = SIZE - 1; i >= 0; i--) {
+        cout << "| " << i << " |";
+
+        if (q.used[i]) {
+            cout << " ";
+            print_point(q.data[i]);
+        }
+
+        cout << endl;
+    }
 }
 
-int main(void)
-{
-	DequeType queue;
-	init_deque(&queue);
-	for (int i = 0; i < 3; i++) {
-		add_front(&queue, i);
-		deque_print(&queue);
-	}
-	for (int i = 0; i < 3; i++) {
-		delete_rear(&queue);
-		deque_print(&queue);
-	}
-	return 0;
+int main() {
+    Deque list;
+    init_deque(list);
+
+    deque_print(list);
+
+    return 0;
 }
