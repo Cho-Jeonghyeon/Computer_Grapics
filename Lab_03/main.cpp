a46f68b350eacf5c54b405d6b71b6ae8bbc7ee73
Cho-Jeonghyeon <69703120+Cho-Jeonghyeon@users.noreply.github.com>
2026-09-09 12:02:23 +0900

    Lab03 점 거리 정렬과 비교 명령 추가

--- Lab_03/main.cpp
+++ Lab_03/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 #include <string>
 
 using namespace std;
@@ -45,6 +47,18 @@ void print_point(const Point& point) {
     cout << point.x << " " << point.y << " " << point.z;
 }
 
+double distance_origin(const Point& point) {
+    return sqrt(point.x * point.x + point.y * point.y + point.z * point.z);
+}
+
+double distance_points(const Point& p1, const Point& p2) {
+    int x = p1.x - p2.x;
+    int y = p1.y - p2.y;
+    int z = p1.z - p2.z;
+
+    return sqrt(x * x + y * y + z * z);
+}
+
 void deque_print(const Deque& q) {
     for (int i = SIZE - 1; i >= 0; i--) {
         cout << "| " << i << " |";
@@ -56,6 +70,107 @@ void deque_print(const Deque& q) {
 
         cout << endl;
     }
+}
+
+void print_distance_list(const Deque& q) {
+    Point sortData[SIZE];
+    double sortDistance[SIZE];
+    int sortCount = 0;
+
+    for (int i = 0; i < SIZE; i++) {
+        if (q.used[i]) {
+            sortData[sortCount] = q.data[i];
+            sortDistance[sortCount] = distance_origin(q.data[i]);
+            sortCount++;
+        }
+    }
+
+    for (int i = 0; i < sortCount - 1; i++) {
+        for (int j = i + 1; j < sortCount; j++) {
+            if (sortDistance[i] > sortDistance[j]) {
+                Point tempPoint = sortData[i];
+                sortData[i] = sortData[j];
+                sortData[j] = tempPoint;
+
+                double tempDistance = sortDistance[i];
+                sortDistance[i] = sortDistance[j];
+                sortDistance[j] = tempDistance;
+            }
+        }
+    }
+
+    for (int i = SIZE - 1; i >= 0; i--) {
+        cout << "| " << i << " |";
+
+        if (i < sortCount) {
+            cout << " ";
+            print_point(sortData[i]);
+            cout << " distance = " << fixed << setprecision(2) << sortDistance[i];
+        }
+
+        cout << endl;
+    }
+}
+
+void print_near_far(const Deque& q) {
+    if (q.count < 2) {
+        cout << "need more points" << endl;
+        return;
+    }
+
+    Point nearPoint1;
+    Point nearPoint2;
+    Point farPoint1;
+    Point farPoint2;
+    double nearDistance = 0;
+    double farDistance = 0;
+    bool firstCheck = true;
+
+    for (int i = 0; i < SIZE; i++) {
+        if (!q.used[i]) {
+            continue;
+        }
+
+        for (int j = i + 1; j < SIZE; j++) {
+            if (!q.used[j]) {
+                continue;
+            }
+
+            double distance = distance_points(q.data[i], q.data[j]);
+
+            if (firstCheck) {
+                nearPoint1 = q.data[i];
+                nearPoint2 = q.data[j];
+                farPoint1 = q.data[i];
+                farPoint2 = q.data[j];
+                nearDistance = distance;
+                farDistance = distance;
+                firstCheck = false;
+            }
+            else if (distance < nearDistance) {
+                nearPoint1 = q.data[i];
+                nearPoint2 = q.data[j];
+                nearDistance = distance;
+            }
+            else if (distance > farDistance) {
+                farPoint1 = q.data[i];
+                farPoint2 = q.data[j];
+                farDistance = distance;
+            }
+        }
+    }
+
+    cout << "near: ";
+    print_point(nearPoint1);
+    cout << " / ";
+    print_point(nearPoint2);
+    cout << " distance = " << fixed << setprecision(2) << nearDistance << endl;
+
+    cout << "far: ";
+    print_point(farPoint1);
+    cout << " / ";
+    print_point(farPoint2);
+    cout << " distance = " << fixed << setprecision(2) << farDistance << endl;
 }
 
 bool add_rear(Deque& q, Point item) {
@@ -156,6 +271,7 @@ int main() {
     init_deque(list);
 
     char command;
+    bool fMode = false;
 
     deque_print(list);
 
@@ -166,22 +282,26 @@ int main() {
             Point point;
             cin >> point.x >> point.y >> point.z;
             add_rear(list, point);
+            fMode = false;
             deque_print(list);
         }
         else if (command == '-') {
             Point removed;
             delete_rear(list, removed);
+            fMode = false;
             deque_print(list);
         }
         else if (command == 'e') {
             Point point;
             cin >> point.x >> point.y >> point.z;
             add_front(list, point);
+            fMode = false;
             deque_print(list);
         }
         else if (command == 'd') {
             Point removed;
             delete_front(list, removed);
+            fMode = false;
             deque_print(list);
         }
         else if (command == 'a') {
@@ -189,11 +309,26 @@ int main() {
         }
         else if (command == 'b') {
             move_down(list);
+            fMode = false;
             deque_print(list);
         }
         else if (command == 'c') {
             init_deque(list);
+            fMode = false;
             deque_print(list);
+        }
+        else if (command == 'f') {
+            fMode = !fMode;
+
+            if (fMode) {
+                print_distance_list(list);
+            }
+            else {
+                deque_print(list);
+            }
+        }
+        else if (command == 'g') {
+            print_near_far(list);
         }
         else if (command == 'q') {
             break;
